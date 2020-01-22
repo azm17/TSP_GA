bb781b74f6c01530d74b0b6263484f65fd1fec42
azm17 <azumi_azumi0917@yahoo.co.jp>
2020-01-23 02:31:04 +0900

    delete_fig

--- Image_merging.py
+++ Image_merging.py
@@ -5,12 +5,12 @@ Created on Thu Jan 23 01:55:41 2020
 @author: Azumi Mamiya
 """
 import cv2
-import numpy as np
 
-
-for i in range(100):
+for i in range(1500):
+    print(i//5)
+    im1 = cv2.imread('fig_fitness/{}.png'.format(i//5))
+    
     image_name = f'{i}.png'
-    im1 = cv2.imread('fig_fitness/'+image_name)
     im2 = cv2.imread('graph_best/'+image_name)
     im3 = cv2.imread('graph/'+image_name)
     
